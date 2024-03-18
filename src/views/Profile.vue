29ad13fb9a1bb87e181e0c4fa28071491bceac5a
Влад Кот <VanderOfCat@gmail.com>
2024-03-18 13:21:45 +0300

    namechange

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -2,11 +2,22 @@
     <div>
         <div class="content">
             <div v-if="user != null">
-                <h3 class="profile-header">
+                <h3 class="profile-header" v-if="!editMode">
                     <div>{{ user.name }}</div>
                     <div>
-                        <a v-if="isThisUserLoggedIn" style="height:48px; display: flex;">
+                        <a href="#" @click="startEdit" v-if="isThisUserLoggedIn" style="height:48px; display: flex;">
                             <SvgIcon :path="mdiPencilOutline" type="mdi" size="48"/>
+                        </a>
+                    </div>
+                </h3>
+                <h3 class="profile-header" v-else>
+                    <input type="text" v-model="text" :placeholder="user.name" :style="(!nameAvailable)?'color:red;':''"/>
+                    <div>
+                        <a href="#" :style="(!nameAvailable)?'opacity:0.2':''" v-if="text != ''" style="height:48px; display: flex;" @click="changeName">
+                            <SvgIcon :path="mdiCheck" type="mdi" size="48"/>
+                        </a>
+                        <a href="#" @click="closeEdit" v-else style="height:48px; display: flex;">
+                            <SvgIcon :path="mdiClose" type="mdi" size="48"/>
                         </a>
                     </div>
                 </h3>
@@ -27,16 +38,19 @@
 import Footer from '@/components/Footer.vue';
 import LoadingSpinner from '@/components/LoadingSpinner.vue'
 import SvgIcon from '@jamescoyle/vue-icon';
-import { mdiPencilOutline } from '@mdi/js'
+import { mdiPencilOutline, mdiClose, mdiCheck } from '@mdi/js'
 
 import { onMounted, ref, computed } from 'vue';
+import { computedAsync } from '@vueuse/core'
 import axios from 'axios';
 import { useUserStore } from '@/stores/user'
 import { useRoute } from 'vue-router';
 
 const user = ref()
+const editMode = ref(false)
 const store = useUserStore()
 const route = useRoute()
+const text = ref("");
 
 const isThisUserLoggedIn = computed(()=>{
     return route.params.uuid==store.uuid;
@@ -45,8 +59,39 @@ const isThisUserLoggedIn = computed(()=>{
 onMounted(async ()=>{
     const res = await fetch("https://goldenoak.vanderc.at/session/minecraft/profile/"+route.params.uuid);
 
-    user.value = await res.json();
+    user.value = await res.json(); //todo: change store too
 })
+
+const nameAvailable = computedAsync(async ()=>{
+    if (text.value == "") return true;
+    const res = await fetch("https://goldenoak.vanderc.at/checkname/"+text.value);
+
+    const val = await res.json();
+    return val.status == "AVAILABLE";
+})
+
+async function startEdit() {
+    editMode.value = true;
+}
+
+async function changeName() {
+    editMode.value = false;
+    if (!nameAvailable) {
+        return
+    }
+    const res = await axios.put("https://goldenoak.vanderc.at/changename/"+text.value, null, {
+        headers: {
+            Authorization: `Bearer ${store.token}`
+        }
+    })
+    store.user.name = text.value
+    user.name.value = text.value
+}
+
+async function closeEdit() {
+    editMode.value= false
+}
+
 </script>
 
 <style scoped>
